dfb2c696afddf4983373d81d455c0f9f854b539d
Caio Rebert <caiorebert070802.cr@gmail.com>
2023-01-15 00:58:18 -0300

    corrigndo queries.js

--- queries/queries.js
+++ queries/queries.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool
 require('dotenv').config();
 const pool = new Pool({
   user: process.env.DB_USER,
-  host: 'postgres://dpg-cf1j8rirrk0bpp942gu0-a.oregon-postgres.render.com',
+  host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
