aa7efe1620f6231b36916dccf3dfbc74628fa5bc
Caio Rebert <caiorebert070802.cr@gmail.com>
2023-01-14 23:45:29 -0300

    config env

--- queries/queries.js
+++ queries/queries.js
@@ -1,10 +1,11 @@
 const Pool = require('pg').Pool
+require('dotenv').config();
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'send2you',
-  password: '1234',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 })
 
 const getPublicacoes = (request, response) => {
